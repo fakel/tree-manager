9ef1ff28929a069247a6117823e333284340d0c8
Fares A. Akel C <fares@startblock.tech>
2020-10-04 06:41:00 -0500

    Add Quasar extras

--- quasar.conf.js
+++ quasar.conf.js
@@ -32,7 +32,7 @@ module.exports = function (/* ctx */) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      // 'ionicons-v4',
+      'ionicons-v4',
       // 'mdi-v5',
       // 'fontawesome-v5',
       // 'eva-icons',
@@ -86,7 +86,13 @@ module.exports = function (/* ctx */) {
     framework: {
       iconSet: 'material-icons', // Quasar icon set
       lang: 'en-us', // Quasar language pack
-      config: {},
+      config: {
+        loadingBar: {
+          color: 'blue',
+          size: '15px',
+          position: 'bottom',
+        },
+      },
 
       // Possible values for "importStrategy":
       // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
@@ -101,7 +107,9 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: [
+        'LoadingBar',
+      ],
     },
 
     // animations: 'all', // --- includes all animations
