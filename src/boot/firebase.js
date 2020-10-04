398bad2115e68320703558653b570ddf14fc6195
Fares A. Akel C <fares@startblock.tech>
2020-10-04 06:55:40 -0500

    Add firebase boot file

---
+++ src/boot/firebase.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from 'firebase/app';
+
+// Add the Firebase services that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAA2uoOw--P7s6bvFLzw2jhvifQuhmLFHo',
+  authDomain: 'patanemo-tree-manager.firebaseapp.com',
+  databaseURL: 'https://patanemo-tree-manager.firebaseio.com',
+  projectId: 'patanemo-tree-manager',
+  storageBucket: 'patanemo-tree-manager.appspot.com',
+  messagingSenderId: '348292456688',
+  appId: '1:348292456688:web:302935b7a03464b6c66235',
+  measurementId: 'G-YW6LMZGMD1',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+Vue.prototype.$auth = firebase.auth();
+Vue.prototype.$db = firebase.firestore();
