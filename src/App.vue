d59183793da992e50c0cbb39a1c0ed660ede488b
Fares A. Akel C <fares@startblock.tech>
2020-10-05 08:00:01 -0500

    Add authentication guard

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,16 @@
 <script>
 export default {
   name: 'App',
+  mounted() {
+  //   this.$idb.getUser()
+  //     .then((res) => {
+  //       console.log(res);
+  //       if (res[0].value.uid) this.$router.push({ path: '/dashboard' });
+  //     });
+    this.$auth().onAuthStateChanged((user) => {
+      if (user) this.$router.push({ path: '/dashboard' });
+      else this.$router.push({ path: '/' });
+    });
+  },
 };
 </script>
