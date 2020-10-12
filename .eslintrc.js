2d0f81a5443a0642fa10c9f2eb220112feba0d57
Fares A. Akel C <fares@startblock.tech>
2020-10-12 02:39:55 -0500

    Include prettier with config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,8 +25,9 @@ module.exports = {
     // See https://eslint.vuejs.org/rules/#available-rules
     'plugin:vue/essential', // Priority A: Essential (Error Prevention)
     // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
+    //'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    'plugin:prettier-vue/recommended',
+    'prettier/vue',
     'airbnb-base'
 
   ],
@@ -47,6 +48,67 @@ module.exports = {
     chrome: true
   },
 
+  settings: {
+    'prettier-vue': {
+      // Settings for how to process Vue SFC Blocks
+      SFCBlocks: {
+        /**
+         * Use prettier to process `<template>` blocks or not
+         *
+         * If set to `false`, remember not to `extends: ['prettier/vue']`, as you need the rules from `eslint-plugin-vue` to lint `<template>` blocks
+         *
+         * @default true
+         */
+        template: false,
+ 
+        /**
+         * Use prettier to process `<script>` blocks or not
+         *
+         * @default true
+         */
+        script: false,
+ 
+        /**
+         * Use prettier to process `<style>` blocks or not
+         *
+         * @default true
+         */
+        style: true,
+ 
+        // Settings for how to process custom blocks
+        customBlocks: {
+          // Treat the `<docs>` block as a `.markdown` file
+          docs: { lang: 'markdown' },
+ 
+          // Treat the `<config>` block as a `.json` file
+          config: { lang: 'json' },
+ 
+          // Treat the `<module>` block as a `.js` file
+          module: { lang: 'js' },
+ 
+          // Ignore `<comments>` block (omit it or set it to `false` to ignore the block)
+          comments: false,
+ 
+          // Other custom blocks that are not listed here will be ignored
+        },
+      },
+ 
+      // Use prettierrc for prettier options or not (default: `true`)
+      usePrettierrc: true,
+ 
+      // Set the options for `prettier.getFileInfo`.
+      // @see https://prettier.io/docs/en/api.html#prettiergetfileinfofilepath-options
+      fileInfoOptions: {
+        // Path to ignore file (default: `'.prettierignore'`)
+        // Notice that the ignore file is only used for this plugin
+        ignorePath: '.testignore',
+ 
+        // Process the files in `node_modules` or not (default: `false`)
+        withNodeModules: false,
+      },
+    },
+  },
+
   // add your custom rules here
   rules: {
     'no-param-reassign': 'off',
@@ -64,6 +126,18 @@ module.exports = {
 
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // prettier
+    'prettier-vue/prettier': [
+      'error',
+      {
+        // Override all options of `prettier` here
+        // @see https://prettier.io/docs/en/options.html
+        printWidth: 100,
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+      },
+    ],
   }
 }
