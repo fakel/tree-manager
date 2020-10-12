4d66faa665c9e989b124572616cc90088e86bdab
Fares A. Akel C <fares@startblock.tech>
2020-10-12 02:48:34 -0500

    Add offline, email/pw and google logins

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -3,14 +3,48 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="eco"
+          aria-label="Eco"
+        />
         <q-toolbar-title>
-          <q-avatar icon="eco" />
           Forest Manager
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
+      <q-dialog v-model="prompt" persistent>
+        <q-card style="min-width: 350px">
+
+          <q-card-section class="q-pt-md q-gutter-md">
+            <q-input v-model="email" filled type="email" hint="Email" />
+            <q-input
+              v-model="password"
+              filled
+              :type="isPwd ? 'password' : 'text'"
+              hint="Password"
+              class="q-pl-none"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+          </q-card-section>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Cancel" v-close-popup />
+            <q-btn flat label="Accept" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
         <q-page padding class="column no-wrap items-center justify-center q-pa-md q-gutter-sm">
             <div class="row col-12">
                 <q-avatar
@@ -21,11 +55,11 @@
                 icon="person" />
             </div>
             <div class="row">
-                <q-btn color="primary" label="Offline Access" />
+                <q-btn @click="offline" color="primary" label="Offline Access" />
             </div>
             <p class="row q-mb-none">Or</p>
             <div class="row">
-                <q-btn color="secondary" label="Sign-In" />
+                <q-btn @click="prompt=true" color="secondary" label="Sign-In" />
             </div>
             <div class="row">
                 <p>Alternatively you can log in using:</p>
@@ -60,10 +94,31 @@
 export default {
   data() {
     return {
+      prompt: false,
+      isPwd: true,
+      email: '',
+      password: '',
     };
   },
   // eslint-disable-next-line no-unused-vars
   methods: {
+    offline() {
+      this.$store.dispatch('base/saveUserState', { userState: 'offline' });
+      this.$router.push({ path: '/dashboard/offline' });
+    },
+    signWithEmailAndPassword() {
+      this.$auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$store.dispatch('base/saveUserState', { userState: 'online' });
+        });
+      // .catch((error) => {
+      //   // Handle Errors here.
+      //   const errorCode = error.code;
+      //   const errorMessage = error.message;
+      //   // ...
+      // });
+    },
     signWithFacebook() {
       console.log('Facebook');
     },
@@ -73,24 +128,16 @@ export default {
     signWithGoogle() {
       console.log('Google');
       const provider = new this.$firebase.auth.GoogleAuthProvider();
-      this.$auth().signInWithPopup(provider).then(() => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const token = result.credential.accessToken;
-        // The signed-in user info.
-        // const { user } = result;
-        // this.$idb.saveUser(user);
-        // this.$store.dispatch('base/storeUser', user);
-      }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-        // The email of the user's account used.
-        // const { email } = error;
-        // The firebase.auth.AuthCredential type that was used.
-        // const { credential } = error;
-        // ...
-      });
+      this.$auth()
+        .signInWithPopup(provider)
+        .then(() => {
+          this.$store.dispatch('base/saveUserState', { userState: 'online' });
+        });
+      //   .catch((error) => {
+      //     const errorCode = error.code;
+      //     const errorMessage = error.message;
+      //     console.log(errorCode, errorMessage);
+      //   });
     },
   },
 };
