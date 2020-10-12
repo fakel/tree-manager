7254ffa34b6f5c6a6f0a5f34310a3fd96446d64d
Fares A. Akel C <fares@startblock.tech>
2020-10-12 02:51:08 -0500

    Add tree info view boilerplate

---
+++ src/pages/treeInfo/Info.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page-container>
+        <q-page padding class="column no-wrap items-center justify-center q-pa-md q-gutter-sm">
+            <h1>Tree Info</h1>
+            <h2>{{getTreeID}}</h2>
+        </q-page>
+    </q-page-container>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+
+const { mapGetters } = createNamespacedHelpers('base');
+
+export default {
+  computed: {
+    ...mapGetters(['getTreeID']),
+  },
+};
+</script>
