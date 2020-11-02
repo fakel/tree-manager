21a76a4f3bbd5795d6de83c71adebd508ef73d76
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-11-02 03:55:56 -0500

    Include Send View

---
+++ src/pages/treeInfo/Send.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page-container>
+    <q-page
+      padding
+      class="column no-wrap items-center justify-center q-pa-md q-gutter-sm"
+    >
+      <q-icon name="cloud_upload" size="150px" />
+      <div class="row q-mb-md">
+        <q-btn style="width: 150px" color="primary" label="Sync with Cloud" />
+      </div>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script>
+export default {};
+</script>
