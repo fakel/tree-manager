b81c364b81da6baab6ccf78d59736d5f75bf03fd
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-11-02 10:25:01 -0500

    Remove consol.log

--- src/pages/treeInfo/Healt.vue
+++ src/pages/treeInfo/Healt.vue
@@ -54,7 +54,6 @@ export default {
       const id = this.getTreeID;
       this.$idb.getEntry('treeInfo', id)
         .then((treeData) => {
-          console.log('treeData', treeData);
           if (!treeData.healt) {
             treeData.healt = [];
           }
